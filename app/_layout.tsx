e462c6e77c3b64f5cf7d45a67b7dcfe113e1ed1f
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-06-12 13:45:23 +0100

    Navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -44,6 +44,7 @@ function RootLayoutNav() {
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+          <Stack.Screen name='tweet/[id]' options={{ title: 'Tweet' }}/>
         </Stack>
       </ThemeProvider>
     </>
